985c2341782d915f8ee542e2040d845704c7f0b8
JuSeRDev <juserdev@gmail.com>
2026-04-15 20:54:09 -0500

    correccion de iconos en sidebar

--- src/app/shared/config/icons.config.ts
+++ src/app/shared/config/icons.config.ts
@@ -1,12 +1,12 @@
 import type { ICON_ASSETS } from "@/app/shared/types/icons.types";
-import quote_settings from '@/assets/icons/icon_quote_settings.svg'
-import client from '@/assets/icons/icon-clients.svg'
-import edit from '@/assets/icons/icon-edit.svg'
-import home from '@/assets/icons/icon-home.svg'
-import products from '@/assets/icons/icon-products.svg'
-import profile from '@/assets/icons/icon-profile.svg'
-import quotation from '@/assets/icons/icon-quotations.svg'
-import remove from '@/assets/icons/icon-delete.svg'
+import client from '@/assets/icons/icon-clients.svg?raw';
+import remove from '@/assets/icons/icon-delete.svg?raw';
+import edit from '@/assets/icons/icon-edit.svg?raw';
+import home from '@/assets/icons/icon-home.svg?raw';
+import products from '@/assets/icons/icon-products.svg?raw';
+import profile from '@/assets/icons/icon-profile.svg?raw';
+import quotation from '@/assets/icons/icon-quotations.svg?raw';
+import quote_settings from '@/assets/icons/icon_quote_settings.svg?raw';
 
 
 export const icons: ICON_ASSETS = {
